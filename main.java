0a43a6c40e2899d4541fc44ab12d1bc44ad67579
Namir Khan <namirk.789@gmail.com>
2025-08-06 14:58:37 +0530

    Updated main class

--- main.java
+++ main.java
@@ -1,14 +1,15 @@
-// package model;
+package model;
 
 public class main {
-    public static void main(String[] args){
-        UserInput user=new UserInput();
+    public static void main(String[] args) {
+        UserInput user = new UserInput();
         user.collectDetails();
-        double calories=CalorieCalculator.calculateTotalCalories(user);
+
         double totalCalories = CalorieCalculator.calculateTotalCalories(user);
-        CalorieCalculator.showCalorieTable(totalCalories);
-        System.out.printf("\n%s, your estimated daily calories need is:%f calories. \n ",
-                user.name.substring(0, 1).toUpperCase() + user.name.substring(1),
-                calories);
+
+        // Display fancy summary and table
+        CalorieCalculator.showResultBox(user, totalCalories);
+        CalorieCalculator.showZigzagCalorieTable(totalCalories);
+        CalorieCalculator.showZigzagSchedule(totalCalories * 0.91, totalCalories * 0.81, totalCalories * 0.62);
     }
 }
